0816e36b71ca6975689f22e4ab380416f096591b
ivorsimstilt <39617577+ivorsimstilt@users.noreply.github.com>
2018-10-24 10:18:03 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ var ploader = new PIXI.loaders.Loader();
 
 function init(){
   console.log('dasdsdasd');
-  ploader.add('fg', 'img/elephant.jpg');
-  ploader.add('depth', 'img/elephant_depth.jpg');
+  ploader.add('fg', 'img/splash_1.jpg');
+  ploader.add('depth', 'img/splash_1_depth.jpg');
 
   ploader.once('complete', startMagic);
   // Begin loading -
